784fcc856a6b87941b2c36d40426a29204f5df8a
Balázs Hajkó <bhajko@gmail.com>
2018-10-09 18:02:10 +0200

    home page slider

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 550,
+    interval: 5000
+});
